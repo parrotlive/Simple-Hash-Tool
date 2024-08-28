2dbab9fe7295aff9f8c75f4296f192deaffab5aa
parrot.live <72553237+parrotlive@users.noreply.github.com>
2024-08-28 19:37:19 +0200

    Added new menu

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 import os
-
+from turtle import back
 os.makedirs('logs', exist_ok=True)
-
 import hashcalc
+import tkinter
 
-hashcalc.hashcalc()
+def runprogram(mode):
+    window.destroy()
+    mode()
+
+window = tkinter.Tk()
+window.title("Hash Calculator")
+selecttext = tkinter.Label(window, text="Select Operation:")
+selecttext.pack()
+hashcalcop = tkinter.Button(window, text="Run Hash Calculator", command=lambda: runprogram(hashcalc.hashcalc))
+hashcalcop.pack()
+tkinter.mainloop()
